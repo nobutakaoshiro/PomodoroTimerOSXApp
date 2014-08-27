9729c3eb460205e4fde0267980bfb7091cd1dad7
Nobutaka OSHIRO <n-oshiro@nekojarashi.com>
2014-08-27 15:04:28 +0900

    Initial Commit

---
+++ PomodoroTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PomodoroTimer
+//
+//  Created by Nobutaka OSHIRO on 2014/08/27.
+//  Copyright (c) 2014年 Nekojarashi Inc. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
